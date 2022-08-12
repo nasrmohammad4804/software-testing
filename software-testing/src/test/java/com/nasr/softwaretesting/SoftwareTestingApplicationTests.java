61d24f35d5d8872a7eef73022401ec08ecc048b7
mohammad <nasrmohammad4804@gmail.com>
2022-08-12 18:05:19 +0430

    add SoftwareTestingApplicationTest

---
+++ software-testing/src/test/java/com/nasr/softwaretesting/SoftwareTestingApplicationTests.java
@@ -0,0 +1,18 @@
+package com.nasr.softwaretesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareTestingApplicationTests {
+
+	/*
+	* integration test -> test entire of application for example at first we create customer with put request and
+	 * then save testing for save customer payment
+	* */
+
+	@Test
+	void contextLoads() {
+	}
+
+}
